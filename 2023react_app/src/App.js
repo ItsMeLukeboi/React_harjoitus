2dbb2db9271e2cb0bd85db709bbb2f4706a4b04c
Lahtinen <luuklah@gmail.com>
2023-02-24 17:29:12 +0200

    napit

--- 2023react_app/src/App.js
+++ 2023react_app/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import {Message} from "./pages/Message";
+import {Buttons} from "./pages/Buttons";
 
 function App() 
 {
   return (
     <div >
       <Message/>
+      <Buttons/>
     </div>
   );
 }
