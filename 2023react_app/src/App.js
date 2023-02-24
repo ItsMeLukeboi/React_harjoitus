d9d9d80d1016d8d7b29eae0ed653e3b479712a24
Lahtinen <luuklah@gmail.com>
2023-02-24 18:23:33 +0200

    ROUTER JUTUT

--- 2023react_app/src/App.js
+++ 2023react_app/src/App.js
@@ -1,14 +1,22 @@
 import React from "react";
 import {Message} from "./pages/Message";
 import {Buttons} from "./pages/Buttons";
+import {Router} from "./pages/Router";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() 
 {
   return (
-    <div >
-      <Message/>
-      <Buttons/>
-    </div>
+    <BrowserRouter>
+      <div >
+        <Routes>
+          <Route path='/' element={<Router/>}>
+            <Route index element={<Message/>}/>
+            <Route path='Buttons' element={<Buttons/>}/>
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
