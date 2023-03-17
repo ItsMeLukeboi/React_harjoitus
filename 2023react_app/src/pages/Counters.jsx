76e60e080a0fb9fe791cf98b09b509199b9b2745
Lahtinen <luuklah@gmail.com>
2023-03-17 18:16:14 +0200

    countter finished and added ItemDatabase.js

--- 2023react_app/src/pages/Counters.jsx
+++ 2023react_app/src/pages/Counters.jsx
@@ -4,15 +4,32 @@ import {useState, useEffect} from "react"
 export const Counters = () =>
 {
     const[count, setCount] = useState(0);
+    const[count2, setCount2] = useState(0);
+    const[count3, setCount3] = useState(0);
+
 
     useEffect(()=>{
-        setCount(count+1)
-    })
+        setTimeout(function(){
+        setCount(count+1);
+       },1000)
+    },[]);
+
+    useEffect(()=>{
+        setTimeout(function(){
+        setCount2(count2+1);
+       },1000)
+
+    });
+
+    useEffect(()=>{
+        setCount3(count3+1);
+    },[count2])
 
     return(
     <div>
         <h1>Time: {count}</h1>
-        
+        <h2>Timer: {count2}</h2> 
+        <h1>Rendered: {count3}</h1>
     </div>
     );
 }
---
+++ 2023react_app/src/pages/ItemDatabase.js
@@ -0,0 +1,12 @@
+export const items =[
+    {
+        img:"https://www.audiworld.com/wp-content/uploads/2022/01/272714635_4963124437082272_7504571078871736700_n-Cropped.jpg"
+        ,
+        id:0,
+    },
+    {
+        img:"https://www.wallpaperflare.com/static/54/968/1004/car-audi-rs-3-lms-red-black-wallpaper.jpg"
+        ,
+        id:1,
+    }
+]
