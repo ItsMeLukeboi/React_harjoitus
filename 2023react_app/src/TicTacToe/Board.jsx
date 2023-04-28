b327ca4948a310ca7db1c899334c60cc39e62413
Lahtinen <luuklah@gmail.com>
2023-04-28 18:16:15 +0300

    TicTacToe borders and .game thing

--- 2023react_app/src/TicTacToe/Board.jsx
+++ 2023react_app/src/TicTacToe/Board.jsx
@@ -6,19 +6,19 @@ export const Board =()=>{
     return(
         <div>
             <div className="row">
-            <Square/>
-            <Square/>
-            <Square/>
+            <Square className="b-bottom-r"/>
+            <Square className="b-bottom-r"/>
+            <Square className="b-bottom"/>
             </div>
             <div className="row">
-            <Square/>
-            <Square/>
-            <Square/>
+            <Square className="b-bottom-r"/>
+            <Square className="b-bottom-r"/>
+            <Square className="b-bottom"/>
             </div>
             <div className="row">
-            <Square/>
-            <Square/>
-            <Square/>
+            <Square className="b-right"/>
+            <Square className="b-right"/>
+            <Square />
             </div>
            
         </div>
--- 2023react_app/src/TicTacToe/TicTacToe.jsx
+++ 2023react_app/src/TicTacToe/TicTacToe.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import './TicTacToe.css';
 import { Board } from "./Board";
+import { useState } from "react"
 export const TicTacToe =()=>{
     return(
         <div>
+            <div className="game"> 
             <h1>Tic-Tac-Toe</h1>
             <Board/>
+            </div>
         </div>
     )
 
